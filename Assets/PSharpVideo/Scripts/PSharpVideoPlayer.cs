601a6009d31e80f0806376e3486343396c834abd
Pokéduel <breckin.bentch@gmail.com>
2025-06-28 17:49:43 -0400

    Meta Files

--- Assets/PSharpVideo/Scripts/PSharpVideoPlayer.cs
+++ Assets/PSharpVideo/Scripts/PSharpVideoPlayer.cs
@@ -14,11 +14,12 @@ namespace PSharpVideo
 		
 		[PSharpSynced(SyncType.Automatic, nameof(OnMasterLockChanged))] public bool isMasterLocked;
 		[PSharpSynced(SyncType.Automatic)] public string video;
+		[PSharpSynced(SyncType.Automatic, nameof(OnPlaybackChanged))] public float playbackRate = 1f;
 
 		[Space]
 		[Space]
 
-		public PVR_VideoProvider videoProvider;
+		public PVR_AVProVideoProvider videoProvider;
 
 		[Space]
 		[Space]
@@ -38,6 +39,7 @@ namespace PSharpVideo
 		public Button muteButton;
 		public Button pauseResumeButton;
 		public Slider timelineSlider;
+		public Slider playbackSlider;
 
 		[Space]
 		
@@ -54,7 +56,7 @@ namespace PSharpVideo
 
 		[Space]
 
-		public AudioSource speaker;
+		public AudioSource[] speakers;
 
 
 		public Graphic loopButtonIcon;
@@ -72,67 +74,90 @@ namespace PSharpVideo
 		[PSharpSynced(SyncType.Automatic, nameof(OnPlayPauseChanged))] private bool _isPaused;
 		[PSharpSynced(SyncType.Automatic, nameof(OnLoopChanged))] private bool _isLooping;
 
+		bool newJoinPause;
+
 		[Space]
 		[Header("Style Colors")]
 		
 		public Color buttonActivatedColor = new Color(1f, 1f, 1f, 1f);
-    public Color iconInvertedColor = new Color(1f, 1f, 1f, 1f);
+		public Color iconInvertedColor = new Color(1f, 1f, 1f, 1f);
 		public Color whiteGraphicColor = new Color(0.9433f, 0.9433f, 0.9433f);
-    public Color buttonBackgroundColor = new Color(1f, 1f, 1f, 1f);
+		public Color buttonBackgroundColor = new Color(1f, 1f, 1f, 1f);
 
-		private void OnURLChanged() 
+		private void OnPlaybackChanged()
 		{
+            if (!videoProvider.isLiveStream)
+            {
+                Debug.Log($"Playback rate changed, new value is {playbackRate}");
+				videoProvider.PlaybackRate = playbackRate;
+            }
+			else
+			{
+				videoProvider.PlaybackRate = 1;
+			}
+            
+		}
 
-			if (!string.IsNullOrEmpty(_currentUrl))
+        private void OnURLChanged() 
+		{
+			Debug.Log($"URL Changed: {_currentUrl}");
+			if (!string.IsNullOrWhiteSpace(_currentUrl))
 			{
 				currentVideo.text = _currentUrl;
-				videoProvider.Play(_currentUrl);
+				videoProvider.PlayURL(_currentUrl);
 			}
 		}
 
 		private void OnTimeChanged()
 		{
-			if (videoProvider.isLoadingVideo || IsObjectOwner)
+			
+			if (videoProvider.isLoading || IsOwner || videoProvider.isLiveStream)
 				return;
-	
-			float desync = (float)_time - (float)videoProvider.videoPlayer.time;
+
+			float desync = (float)_time - (float)videoProvider.Time;
 	
 			if (Mathf.Abs(desync) > 4)
 			{
-				videoProvider.videoPlayer.time = _time;
-				if (!videoProvider.videoPlayer.isPlaying && !_isPaused) videoProvider.videoPlayer.Play();
+				videoProvider.Time = _time;
+				if (videoProvider.IsPaused && !_isPaused) videoProvider.IsPaused = false;
 			}
 		}
 
 		private void OnVideoOwnerChanged()
 		{
+			Debug.Log("Video Owner Changed");
 			videoOwner.text = _videoOwnerName;
 		}
 
 		private void OnLastVideoChanged()
 		{
+			Debug.Log("Last Video Changed");
+
 			lastVideo.text = _lastVideo;
 		}
 
 		private void OnPlayPauseChanged()
 		{
+			Debug.Log("PlayPause Changed");
 			if(_isPaused)
 			{
 				pauseResumeIcons[0].SetActive(true);
 				pauseResumeIcons[1].SetActive(false);
-				videoProvider.videoPlayer.Pause();
+				videoProvider.IsPaused = true;
 			}
 			else 
 			{
 				pauseResumeIcons[0].SetActive(false);
 				pauseResumeIcons[1].SetActive(true);
-				videoProvider.videoPlayer.Play();
+				videoProvider.IsPaused = false;
 			}
 		}
 
 		private void OnMasterLockChanged()
 		{
-			if(isMasterLocked)
+			Debug.Log("Master Lock Changed");
+
+			if (isMasterLocked)
 			{
 				masterLockIcons[0].SetActive(true);
 				masterLockIcons[1].SetActive(false);
@@ -183,74 +208,61 @@ namespace PSharpVideo
 
 		private void OnLoopChanged()
 		{
-			if(_isLooping)
+
+			if (_isLooping)
 			{
 				if(loopButtonBackground) loopButtonBackground.color = buttonActivatedColor;
 				if(loopButtonIcon) loopButtonIcon.color = iconInvertedColor;
+				videoProvider.Loop = true;
 			}
 			else
 			{
 				if(loopButtonBackground) loopButtonBackground.color = buttonBackgroundColor;
 				if(loopButtonIcon) loopButtonIcon.color = whiteGraphicColor;
-			}
+                videoProvider.Loop = false;
+            }
+		}
+
+		private void AddVideo(string url)
+		{
+
+			_currentUrl = url;
 		}
 
 		public void Awake()
 		{
+			
 			urlInput.onSubmit.AddListener((url) => 
 			{
-				if(!IsObjectOwner)
-					PSharpNetworking.SetOwner(PSharpPlayer.LocalPlayer, gameObject);
-				if(isMasterLocked)
-				{
-					if(PSharpPlayer.LocalPlayer.IsMaster)
-					{
-						_videoOwnerName = PSharpPlayer.LocalPlayer.Username;
-						_time = 0;
-						if (!string.IsNullOrEmpty(_currentUrl)) _lastVideo = _currentUrl;
-						_currentUrl = url;
-					}
-				}
-				else 
-				{
-					_videoOwnerName = PSharpPlayer.LocalPlayer.Username;
-					_time = 0;
-					if (!string.IsNullOrEmpty(_currentUrl)) _lastVideo = _currentUrl;
-					_currentUrl = url;
-				}
-				
 				urlInput.SetTextWithoutNotify(string.Empty);
+				if (!IsOwner)
+				{
+					SendNetworkedEvent(NetworkEventTarget.Owner, nameof(OnRequestAddVideo), url);
+					return;
+				}
+
+				AddVideo(url);
 			});
 			pauseResumeButton.onClick.AddListener(() => 
 			{
-				if(isMasterLocked)
+				if(!IsOwner && !isMasterLocked)
 				{
-					if(PSharpPlayer.LocalPlayer.IsMaster)
-					{
-						if (!IsObjectOwner)
-							PSharpNetworking.SetOwner(PSharpPlayer.LocalPlayer, gameObject);
-						_isPaused = !_isPaused;
-					}
+					SendNetworkedEvent(NetworkEventTarget.Owner, nameof(OnRequestPlayPause));
 					return;
 				}
-				else
-				{
-					if (!IsObjectOwner)
-						PSharpNetworking.SetOwner(PSharpPlayer.LocalPlayer, gameObject);
-					_isPaused = !_isPaused;
-				}
-				
+
+				_isPaused = !_isPaused;
+
 			});
 
 			masterLockButton.onClick.AddListener(() => 
 			{
-
-				if (!IsObjectOwner)
-				{
-					PSharpNetworking.SetOwner(PSharpPlayer.LocalPlayer, gameObject);	
-				}
 				if(PSharpPlayer.LocalPlayer.IsMaster)
-						isMasterLocked = !isMasterLocked;
+				{
+					PSharpNetworking.SetOwner(PSharpPlayer.LocalPlayer, gameObject);
+					isMasterLocked = !isMasterLocked;
+				}
+						
 			});
 
 			muteButton.onClick.AddListener(() => 
@@ -259,15 +271,14 @@ namespace PSharpVideo
 				
 				if(_isMuted)
 				{
-					Debug.Log("Muted? Yes");
-					speaker.mute = true;
+					videoProvider.Mute(true);
+						
 					muteIcons[0].SetActive(false);
 					muteIcons[1].SetActive(true);
 				}
 				else {
-					Debug.Log("Muted? No");
-					speaker.mute = false;
-					muteIcons[0].SetActive(true);
+                    videoProvider.Mute(false);
+                    muteIcons[0].SetActive(true);
 					muteIcons[1].SetActive(false);
 				}
 
@@ -275,31 +286,23 @@ namespace PSharpVideo
 
 			reloadButton.onClick.AddListener(() =>
 			{
-				if (!IsObjectOwner)
+				if (!IsOwner)
 					PSharpNetworking.SetOwner(PSharpPlayer.LocalPlayer, gameObject);
 				ReloadVideo();
 			});
 
 			loopButton.onClick.AddListener(() =>
 			{
-				if(isMasterLocked)
+				if (!IsOwner && !isMasterLocked)
 				{
-					if(PSharpPlayer.LocalPlayer.IsMaster)
-					{
-						if(!IsObjectOwner)
-							PSharpNetworking.SetOwner(PSharpPlayer.LocalPlayer, gameObject);
-						_isLooping = !_isLooping;
-					}
+					SendNetworkedEvent(NetworkEventTarget.Owner, nameof(OnRequestLoopButton));
 					return;
 				}
-				else
-				{
-					if(!IsObjectOwner)
-						PSharpNetworking.SetOwner(PSharpPlayer.LocalPlayer, gameObject);
-					_isLooping = !_isLooping;
-				}
-				
+
+				_isLooping = !_isLooping;
+
 			});
+
 			infoButton.onClick.AddListener(() => 
 			{
 				foreach (GameObject obj in infoObjects)
@@ -307,34 +310,73 @@ namespace PSharpVideo
 					obj.SetActive(!obj.activeSelf);
 				}
 			});
+
 			timelineSlider.onValueChanged.AddListener((v) =>
 			{
 				if(isMasterLocked)
 				{
 					if(PSharpPlayer.LocalPlayer.IsMaster)
 					{
-						if (!IsObjectOwner)
+						if (!IsOwner)
 							PSharpNetworking.SetOwner(PSharpPlayer.LocalPlayer, gameObject);
 
-						videoProvider.videoPlayer.time = videoProvider.videoPlayer.length * v;
+						videoProvider.Time = videoProvider.duration * v;
 					}
 					return;
 				}
 				else
 				{
-					if (!IsObjectOwner)
+					if (!IsOwner)
 						PSharpNetworking.SetOwner(PSharpPlayer.LocalPlayer, gameObject);
 
-					videoProvider.videoPlayer.time = videoProvider.videoPlayer.length * v;
+					videoProvider.Time = videoProvider.duration * v;
 				}
 				
 			});
 
+			if(playbackSlider != null)
+			{
+				playbackSlider.onValueChanged.AddListener((v) =>
+				{
+					if(!videoProvider.isLiveStream)
+					{
+						if (isMasterLocked)
+						{
+							if(PSharpPlayer.LocalPlayer.IsMaster || !IsOwner)
+							{
+								if(!IsOwner)
+								{
+									PSharpNetworking.SetOwner(PSharpPlayer.LocalPlayer, gameObject);
+								}
+
+								playbackRate = playbackSlider.value;
+							}	
+						}
+						else
+						{
+							if (!IsOwner)
+							{
+								PSharpNetworking.SetOwner(PSharpPlayer.LocalPlayer, gameObject);
+							}
+
+							playbackRate = playbackSlider.value;
+						}
+					}
+					else
+					{
+						playbackSlider.value = 1;
+						playbackRate = 1;
+					}
+				
+				});
+			}
+			
+
 			videoProvider.OnVideoLoading += () =>
 			{
-				videoProvider.videoPlayer.Stop();
+				videoProvider.Stop();
 
-
+				playbackSlider.interactable = false;
 				pauseResumeButton.interactable = false;
 				loopButton.interactable = false;
 				reloadButton.interactable = false;
@@ -344,7 +386,8 @@ namespace PSharpVideo
 
 			videoProvider.OnVideoError += () =>
 			{
-				pauseResumeButton.interactable = true;
+                playbackSlider.interactable = false;
+                pauseResumeButton.interactable = true;
 				loopButton.interactable = true;
 				reloadButton.interactable = true;
 
@@ -353,34 +396,67 @@ namespace PSharpVideo
 
 			videoProvider.OnVideoReady += () =>
 			{
-				pauseResumeButton.interactable = true;
+                if (videoProvider.isLiveStream)
+                {
+                    playbackSlider.interactable = false;
+                }
+                else
+                {
+                    playbackSlider.interactable = true;
+                }
+
+                playbackSlider.value = 1;
+                playbackRate = 1;
+                pauseResumeButton.interactable = true;
 				loopButton.interactable = true;
 				reloadButton.interactable = true;
 			};
 
 			videoProvider.OnVideoStarted += () =>
 			{
-				statusText.text = string.Empty;
-				
-				videoProvider.videoPlayer.time = _time;
-				_isPaused = false;
-				_videoLength = GetTime(videoProvider.videoPlayer.frameCount / videoProvider.videoPlayer.frameRate);
-				statusText.text = $"{GetTime(videoProvider.videoPlayer.time)} / {_videoLength}";
 
+                statusText.text = string.Empty;
+				
+				videoProvider.Time = _time;
+				_isPaused = false;
+
+				if(newJoinPause)
+				{
+					_isPaused = true;
+					videoProvider.IsPaused = true;
+					newJoinPause = false;
+					_time = videoProvider.Time;
+				}
+                if (!videoProvider.isLiveStream)
+                {
+                    timelineSlider.interactable = true;
+                    pauseResumeButton.interactable = true;
+
+                    _videoLength = GetTime(videoProvider.duration);
+                    SetTimeText(true);
+                }
+                else
+                {
+                    timelineSlider.value = 0;
+                    statusText.text = "";
+
+                    timelineSlider.interactable = false;
+                    pauseResumeButton.interactable = false;
+                }
+
+                if (_isPaused)
+                {
+                    videoProvider.IsPaused = true;
+                }
 			};
 			
 			videoProvider.OnVideoEnd += () =>
 			{
-				if(IsObjectOwner)
+				if(IsOwner)
 				{
-					if(!_isLooping)
+					if(!_isLooping && !videoProvider.isLoading && !videoProvider.isPlaying)
 					{
 						_currentUrl = string.Empty;
-					}
-					else 
-					{
-						_time = 0;
-						videoProvider.videoPlayer.Play();
 					}
 				}
 			};
@@ -388,53 +464,126 @@ namespace PSharpVideo
 			StartCoroutine(Synchronizer());
 		}
 
-		private void ReloadVideo() 
+		[PSharpEvent]
+		private void OnRequestAddVideo(string url)
+		{
+
+			if (!IsOwner) return;
+
+			if (isMasterLocked)
+			{
+				if (PSharpPlayer.LocalPlayer.IsMaster)
+				{
+					_videoOwnerName = PSharpPlayer.LocalPlayer.Username;
+					_time = 0;
+					if (!string.IsNullOrEmpty(_currentUrl)) _lastVideo = _currentUrl;
+					_currentUrl = url;
+				}
+			}
+			else
+			{
+				_videoOwnerName = PSharpPlayer.LocalPlayer.Username;
+				_time = 0;
+				if (!string.IsNullOrEmpty(_currentUrl)) _lastVideo = _currentUrl;
+				_currentUrl = url;
+			}
+		}
+
+		[PSharpEvent]
+		private void OnRequestPlayPause()
+		{
+
+			if (!IsOwner || isMasterLocked)
+			{
+				return;
+			}
+			else
+			{
+				_isPaused = !_isPaused;
+			}
+
+		}
+
+		[PSharpEvent]
+		private void OnRequestLoopButton()
+		{
+
+			if (!IsOwner || isMasterLocked)
+			{ 
+				return;
+			}
+			else
+			{
+				_isLooping = !_isLooping;
+			}
+		}
+
+		private void SetTimeText(bool setSlider)
+		{
+			if (videoProvider.isLiveStream)
+				return;
+
+			statusText.text = $"{GetTime(videoProvider.Time)} / {_videoLength}";
+
+			if (setSlider)
+			{
+				timelineSlider.SetValueWithoutNotify((float)(videoProvider.Time / videoProvider.duration));
+			}
+		}
+
+        private void ReloadVideo() 
 		{
 			if (!string.IsNullOrEmpty(_currentUrl))
 			{
 				reloadButton.GetComponent<Animator>().SetTrigger("Rotate");
-				videoProvider.Play(_currentUrl);
+				videoProvider.PlayURL(_currentUrl);
 			}
 			
 		}
 
-		public IEnumerator Synchronizer()
+        private IEnumerator Synchronizer()
+        {
+            while (true)
+            {
+                if (videoProvider.isPlaying)
+                {
+                    SetTimeText(true);
+
+                    if (IsOwner)
+                    {
+                        _time = videoProvider.Time;
+                    }
+                }
+
+                yield return new WaitForSeconds(1.0f);
+            }
+        }
+
+        public override void OnNetworkReady()
 		{
-			while (true)
+			_masterName = PSharpPlayer.MasterPlayer.Username;
+			masterName.text = _masterName;
+
+			if(!string.IsNullOrEmpty(video) && IsOwner )	_currentUrl = video;
+
+			if(_isPaused)
 			{
-				if (videoProvider.videoPlayer.isPlaying)
-				{
-					statusText.text = $"{GetTime(videoProvider.videoPlayer.time)} / {_videoLength}";
-
-					timelineSlider.SetValueWithoutNotify((float)(videoProvider.videoPlayer.time / videoProvider.videoPlayer.length));
-
-					if (IsObjectOwner)
-					{
-						_time = videoProvider.videoPlayer.time;
-					}
-				}
-
-				yield return new WaitForSeconds(1.0f);
+				newJoinPause = true;
 			}
 		}
 
-    public override void OnNetworkReady()
-    {
-			_masterName = PSharpPlayer.MasterPlayer.Username;
-			masterName.text = _masterName;
-			if(!string.IsNullOrEmpty(video) && IsObjectOwner )	_currentUrl = video;
-    }
-    public override void OnMasterChanged(PSharpPlayer newMaster)
-    {
+		public override void OnMasterChanged(PSharpPlayer newMaster)
+		{
 			_masterName = newMaster.Username;
 			masterName.text = _masterName;
-      OnMasterLockChanged();
-    }
-    public string GetTime(double seconds)
+			OnMasterLockChanged();
+		}
+
+		public string GetTime(double seconds)
 		{
 			TimeSpan timeSpan = TimeSpan.FromSeconds(seconds);
 
 			return timeSpan.Hours > 0 ? timeSpan.ToString(@"h\:mm\:ss") : timeSpan.ToString(@"m\:ss");
 		}
-  }
+	}
 }
